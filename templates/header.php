ba1e93b2743d40fc4202ffcdb93200a69f93608f
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2024-04-03 12:33:22 +0200

    Update header.php

---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang ="cs">
+    <head>
+        <link rel="stylesheet" href=".css/styles.css">
+        <meta charset="UTF - 8">
+    </head>
+
+    <body>
+        <h1>Muj skvely projekt</h1>
+
+
+    </body>
+
+
+
+</html>
