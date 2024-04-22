2f6fa02408fd1d480f9297204b3cef5905f69940
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2024-04-22 10:31:40 +0200

    Styly a nějakej text
    styly u obrazku a nejaky info a text

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
                     <img src ="data/img/krizek.png" alt="prazdno1">
                 </div>
 
-                <div class ="reklama2">
-                    <img src ="data/img/krizek.png" alt="prazdno1">
+                <div class ="reklama3">
+                    <img src ="data/img/krizek.png" alt="prazdno2">
                 </div>
 
                 
@@ -50,17 +50,33 @@
                 
 
                 <div class="text1"> 
-                    <h3>Život</h3>
+                    <img class="img1" src="data/img/logo-medlov1.png" alt="MedlovLogo" width="170" height="200"> 
 
-                    <p>s</p>
-                    <p>s</p>
-                    <p>s</p>
-                    <p>s</p>
-                    <p>s</p>
-                    <p>s</p>
+                    <h3>Medlov</h3>
+
+                    <p>Medlov v okrese Brno-venkov je malá obec s bohatou historií a malebným venkovským prostředím.
+                        Tato vesnice se nachází v blízkosti města Židlchovice a je obklopena překrásnou krajinou, která láká turisty i místní obyvatele.</p>
+                    <p>Vesnice se může pyšnit krásnou sakrální architekturou, včetně kostela či kapliček, 
+                    <p> Jedním z nejvýraznějších prvků Medlova v okrese Brno-venkov je jeho klidná atmosféra a pohodový venkovský život.
+                        Pro milovníky přírody a aktivní odpočinek nabízí okolí vesnice skvělé možnosti pro pěší turistiku, cykloturistiku či rodinné výlety.</p>
+                    <p>Tradiční moravská kuchyně se zde také nabízí v místních restauracích a hospůdkách,
+                         kde si můžeš vychutnat lahodné regionální pokrmy, vína a piva.</p>
+                    <p>Kulturní život v Medlově je také bohatý. 
+                        Vesnice pořádá různé kulturní akce a folklórní festivaly, které přitahují návštěvníky z širokého okolí. 
+                        Tyto akce jsou skvělou příležitostí poznat místní tradice, zvyky a kulturu.</p>
                 </div>
+
                 <div class="text1">
-                    <p>sssssssssssssssssssssssssss</p>
+                    <h4>Poloha</h4>
+                    <p>Medlov se nachází na Jižní Moravě, konkrétně okres Brno-venkov, nachází se přibližně 20 kilometrů od Brna</p>
+                    <p>Medlov leží kousek od dálníce D52, která vede z Brna do Pohořelic</p>
+                    <img class="img2" src="data/img/poloha.png" alt="MedlovPoloha1" width="700" height="400"> 
+                </div>
+
+                <div class="text1">
+                    <h4>Okolní Obce</h4>
+                    <p>V okolí medlova se nachází města jako jsou: Pohořelice, Židlchovice, Rajhrad a 20 kilometrů od Medlova se nachází samotné Brno</p>
+                    <img class="img3" src="data/img/poloha2.png" alt="Poloha2" width="800" height="750">
                 </div>
                 <p>s</p>
                 <p>s</p>
